29501ab8374d174e515dbdc3a89f386bd3a3e30e
diohen90 <diohen90@gmail.com>
2023-02-28 23:12:11 +0000

    First try

---
+++ Challenge04.sh
@@ -0,0 +1,10 @@
+#Print "Hello World!" to the screen
+echo "Hello World!"
+
+#Ping self (Loopback address)
+ping -c 4 127.0.0.1
+
+#IP Info (Print Network adapter from this computer)
+ip link show wlp1s0
+
+exit
