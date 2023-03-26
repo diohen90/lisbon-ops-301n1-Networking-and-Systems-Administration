0cb81643c2d192a0ec40939ecbabb5cccdb68c09
diohen90 <diohen90@gmail.com>
2023-03-27 00:07:02 +0100

    Challenge 13 first try

---
+++ Challenge13.ps1
@@ -0,0 +1,28 @@
+# Set the variables for the new user
+$firstName = "Franz"
+$lastName = "Ferdinand"
+$displayName = "$firstName $lastName"
+$office = "Springfield"
+$state = "OR"
+$department = "TPS Department"
+$jobTitle = "TPS Reporting Lead"
+$email = "ferdi@GlobeXpower.com"
+$username = "ferdinand"
+
+# Generate a secure password for the user
+$securePassword = ConvertTo-SecureString -String "P@ssw0rd123" -AsPlainText -Force
+
+# Create the new user account
+New-ADUser -Name $displayName `
+           -GivenName $firstName `
+           -Surname $lastName `
+           -DisplayName $displayName `
+           -Office $office `
+           -State $state `
+           -Department $department `
+           -Title $jobTitle `
+           -AccountPassword $securePassword `
+           -Enabled $true `
+           -UserPrincipalName "$username@GlobeXpower.com" `
+           -EmailAddress $email `
+           -Path "CN=Users,DC=GlobeXpower,DC=com"
