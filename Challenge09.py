11b19ba1e4750aa2f59f2c39bfc459ec169101a8
diohen90 <diohen90@gmail.com>
2023-04-08 23:15:34 +0100

    First try

---
+++ Challenge09.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+# Declare variables
+a = 5
+b = 10
+
+# Equals
+if a == b:
+    print("a is equal to b")
+    
+# Not Equals
+if a != b:
+    print("a is not equal to b")
+    
+# Less than
+if a < b:
+    print("a is less than b")
+    
+# Less than or equal to
+if a <= b:
+    print("a is less than or equal to b")
+    
+# Greater than
+if a > b:
+    print("a is greater than b")
+    
+# Greater than or equal to
+if a >= b:
+    print("a is greater than or equal to b")
+
+# If/elif statement
+if a == b:
+    print("a is equal to b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is greater than b")
+
+# If/elif/else statement
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
