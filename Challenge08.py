a91c7bfc94576872eb76ed20f85b099db69af7ee
diohen90 <diohen90@gmail.com>
2023-04-08 22:59:45 +0100

    Firs try

---
+++ Challenge08.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Assign a list of ten string elements to a variable
+my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'imbe', 'jackfruit']
+
+# Print the fourth element of the list
+print("Fourth element of the list:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth element of the list:", my_list[5:])
+
+# Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+print("List with the seventh element changed to 'onion':", my_list)
