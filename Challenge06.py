1a943c091910d9d4dfd8afa5764ea3961e52f1dd
diohen90 <diohen90@gmail.com>
2023-04-08 22:31:23 +0100

    Testing

--- Challenge06.py
+++ Challenge06.py
@@ -2,4 +2,21 @@
 
 import os
 
-os.system("whoami")
+# Run the "whoami" command and capture the output
+whoami_output = os.popen('whoami').read().strip()
+
+# Run the "ip a" command and capture the output
+ip_output = os.popen('ip a').read().strip()
+
+# Run the "lshw -short" command and capture the output
+lshw_output = os.popen('lshw -short').read().strip()
+
+# Print the output of the three commands
+print("Output of 'whoami' command:")
+print(whoami_output)
+
+print("Output of 'ip a' command:")
+print(ip_output)
+
+print("Output of 'lshw -short' command:")
+print(lshw_output)
